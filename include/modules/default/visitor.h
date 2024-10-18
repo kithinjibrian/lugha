bf876c6a992a1e80528e7982dacbcf718b5c22ab
kithinjibrian <kithinjibrian369@gmail.com>
2024-10-19 01:22:02 +0300

    default visitors

---
+++ include/modules/default/visitor.h
@@ -0,0 +1,33 @@
+#ifndef VISITOR_H
+#define VISITOR_H
+
+#include "ast.h"
+
+#define define_visitor(_name, _type) \
+    void *_name(node_visitor_t *visitor, _type *ast)
+
+/*
+    Default visitors
+ */
+define_visitor(def_eof, node_ast_t);
+define_visitor(def_entry, node_ast_t);
+define_visitor(def_leave, node_ast_t);
+define_visitor(def_block, node_block_t);
+define_visitor(def_string, node_string_t);
+define_visitor(def_symbol, node_symbol_t);
+define_visitor(def_number, node_number_t);
+define_visitor(def_return, node_return_t);
+define_visitor(def_variable, node_variable_t);
+define_visitor(def_arguments, node_arguments_t);
+define_visitor(def_parameter, node_parameter_t);
+define_visitor(def_expression, node_expression_t);
+define_visitor(def_binary_expression, node_binary_t);
+define_visitor(def_function_dec, node_function_dec_t);
+define_visitor(def_source_elements, node_statements_t);
+define_visitor(def_function_call, node_function_call_t);
+define_visitor(def_variable_list, node_variable_list_t);
+define_visitor(def_parameter_list, node_parameter_list_t);
+define_visitor(def_expression_statement, node_expression_t);
+define_visitor(def_variable_statement, node_variable_statement_t);
+
+#endif /* VISITOR_H */
---
+++ src/modules/default/visitors.c
@@ -0,0 +1,151 @@
+#include "modules/default/visitor.h"
+
+define_visitor(def_block, node_block_t)
+{
+    ast->statements->accept(ast->statements, visitor);
+    return NULL;
+}
+
+define_visitor(def_expression_statement, node_expression_t)
+{
+    ast->expression->accept(ast->expression, visitor);
+    return NULL;
+}
+
+define_visitor(def_expression, node_expression_t)
+{
+    ast->expression->accept(ast->expression, visitor);
+    return NULL;
+}
+
+define_visitor(def_binary_expression, node_binary_t)
+{
+    ast->left->accept(ast->left, visitor);
+    ast->right->accept(ast->right, visitor);
+    return NULL;
+}
+
+define_visitor(def_source_elements, node_statements_t)
+{
+    for (int i = 0; i < ast->nch; i++)
+    {
+        ast->children[i]->accept(ast->children[i], visitor);
+    }
+    return NULL;
+}
+
+define_visitor(def_variable_statement, node_variable_statement_t)
+{
+    ast->statement->accept(ast->statement, visitor);
+    return NULL;
+}
+
+define_visitor(def_variable_list, node_variable_list_t)
+{
+    for (int i = 0; i < ast->nvars; i++)
+    {
+        ast->variables[i]->accept(ast->variables[i], visitor);
+    }
+    return NULL;
+}
+
+define_visitor(def_parameter_list, node_parameter_list_t)
+{
+    for (int i = 0; i < ast->params_no; i++)
+    {
+        ast->parameters[i]->accept(ast->parameters[i], visitor);
+    }
+    return NULL;
+}
+
+define_visitor(def_variable, node_variable_t)
+{
+    ast->identifer->accept(ast->identifer, visitor);
+    if (ast->expression)
+        ast->expression->accept(ast->expression, visitor);
+    return NULL;
+}
+
+define_visitor(def_parameter, node_parameter_t)
+{
+    ast->identifer->accept(ast->identifer, visitor);
+
+    if (ast->expression)
+        ast->expression->accept(ast->expression, visitor);
+    return NULL;
+}
+
+define_visitor(def_function_dec, node_function_dec_t)
+{
+    if (ast->parameters)
+        ast->parameters->accept(ast->parameters, visitor);
+    ast->block->accept(ast->block, visitor);
+    return NULL;
+}
+
+define_visitor(def_function_call, node_function_call_t)
+{
+    ast->expression->accept(ast->expression, visitor);
+    ast->arguments->accept(ast->arguments, visitor);
+    return NULL;
+}
+
+define_visitor(def_arguments, node_arguments_t)
+{
+    for (int i = 0; i < ast->nargs; i++)
+    {
+        ast->args[i]->accept(ast->args[i], visitor);
+    }
+    return NULL;
+}
+
+define_visitor(def_number, node_number_t)
+{
+    (void)visitor;
+    (void)ast;
+    return NULL;
+}
+
+define_visitor(def_symbol, node_symbol_t)
+{
+    (void)visitor;
+    (void)ast;
+    return NULL;
+}
+
+define_visitor(def_string, node_string_t)
+{
+    (void)visitor;
+    (void)ast;
+    return NULL;
+}
+
+define_visitor(def_return, node_return_t)
+{
+    if (ast->expression)
+        ast->expression->accept(ast->expression, visitor);
+
+    return NULL;
+}
+
+define_visitor(def_eof, node_ast_t)
+{
+    (void)visitor;
+    (void)ast;
+    return NULL;
+}
+
+define_visitor(def_entry, node_ast_t)
+{
+    (void)visitor;
+    (void)ast;
+    printf("%s\n", ast->type_str);
+    return NULL;
+}
+
+define_visitor(def_leave, node_ast_t)
+{
+    (void)visitor;
+    (void)ast;
+    return NULL;
+}
